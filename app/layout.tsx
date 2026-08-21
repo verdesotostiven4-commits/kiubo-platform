3ff2e1db28eb97abfea8a21116657eb22d162a05
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-08-20 19:02:15 -0600

    feat: load command palette styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import "./marketing.css";
 import "./sales.css";
 import "./product.css";
 import "./premium.css";
+import "./command.css";
 import { PwaRegister } from "@/components/PwaRegister";
 import { SessionEnforcer } from "@/components/SessionEnforcer";
 import { BrandingRuntime } from "@/components/BrandingRuntime";
