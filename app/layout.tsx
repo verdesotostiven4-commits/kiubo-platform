874ceccbf06fe192271517306815a940976790ce
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-08-27 12:16:43 -0500

    load experience v11 refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ import "./experience-v7.css";
 import "./experience-v8.css";
 import "./product-options-v9.css";
 import "./experience-v10.css";
+import "./experience-v11.css";
 import { PwaRegister } from "@/components/PwaRegister";
 import { SessionEnforcer } from "@/components/SessionEnforcer";
 import { BrandingRuntime } from "@/components/BrandingRuntime";
