060e9a20a3338fc74d14bbd292e14439e2558358
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-09-03 17:46:34 -0500

    style: load experience v14

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ import "./experience-v10.css";
 import "./experience-v11.css";
 import "./experience-v12.css";
 import "./experience-v13.css";
+import "./experience-v14.css";
 import { PwaRegister } from "@/components/PwaRegister";
 import { SessionEnforcer } from "@/components/SessionEnforcer";
 import { BrandingRuntime } from "@/components/BrandingRuntime";
